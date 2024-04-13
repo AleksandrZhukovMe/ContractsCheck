d0db6b042882cf5819608efbdd422474f96f8420
Санёчек <a.zhukov@uzum.com>
2024-04-13 17:33:18 +0200

    initial commit

---
+++ src/main/java/me/eample/inventorycheck/InventoryCheckApplication.java
@@ -0,0 +1,13 @@
+package me.eample.inventorycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/eample/inventorycheck/InventoryCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eample.inventorycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
